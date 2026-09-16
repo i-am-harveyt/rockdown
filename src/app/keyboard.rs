71ff887cf3e889a589b047b6f99d6afe9c1e3893
i-am-harveyt <haotingtong30@gmail.com>
2026-09-16 15:05:13 +0800

    fix: prevent if collapse

--- src/app/keyboard.rs
+++ src/app/keyboard.rs
@@ -161,12 +161,11 @@ impl Workspace {
             }
             return Ok(true);
         }
-        if self.ui_mode == UiMode::Writer && key == "escape" && self.command.is_none() {
-            if self.pane == Pane::Explorer && self.buffer().mode == Mode::Normal {
+        if self.ui_mode == UiMode::Writer && key == "escape" && self.command.is_none()
+            && self.pane == Pane::Explorer && self.buffer().mode == Mode::Normal {
                 self.set_pane(Pane::Editor, cx);
                 return Ok(true);
             }
-        }
         if clipboard_shortcut && stroke.key == "c" && self.pane != Pane::Terminal {
             let buffer = self.buffer();
             let mut selected = buffer
